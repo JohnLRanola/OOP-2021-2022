28471228e92f939225d0daa7ae788866bfa2ac48
JohnLRanola <johnlranola@gmail,com>
2022-02-02 13:32:44 +0000

    killing the cat

--- java/src/ie/tudublin/Cat.java
+++ java/src/ie/tudublin/Cat.java
@@ -6,13 +6,18 @@ public class Cat extends Animal
 
     public void kill()
     {
+<<<<<<< HEAD
         if(numLives > 0 )
+=======
+        if (numLives > 0)
+>>>>>>> f48aec3078eb59df87cccb1b9fae4670b81401f3
         {
             System.out.println("Ouch!");
             numLives --;
         }
         else
         {
+<<<<<<< HEAD
             System.out.println("I'm Dead!");
         }
     }
@@ -29,5 +34,26 @@ public class Cat extends Animal
 
     public void setNumLives(int numLives) {
         this.numLives = numLives;
+=======
+            System.out.println("Im dead");
+        }
+    }
+
+    
+    public int getNumLives() {
+        return numLives;
+    }
+
+
+    public void setNumLives(int numLives) {
+        this.numLives = numLives;
+    }
+
+
+    public Cat(String name)
+    {
+        super(name);
+        numLives = 9;
+>>>>>>> f48aec3078eb59df87cccb1b9fae4670b81401f3
     }
 }
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -2,7 +2,13 @@ package ie.tudublin;
 
 public class Main
 {
-    public static void main(String[] args)
+    public static void helloProcessing()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new HelloProcessing());
+    }
+
+    public void cat()
     {
         System.out.println("Hello world");
 
@@ -17,12 +23,24 @@ public class Main
 
         Cat cat = new Cat("Ginger");
 
+<<<<<<< HEAD
         while(cat.getNumLives() > 0 )
+=======
+        while(cat.getNumLives() > 0)
+>>>>>>> f48aec3078eb59df87cccb1b9fae4670b81401f3
         {
             cat.kill();
         }
         cat.kill();
 
+<<<<<<< HEAD
+=======
+    }
+    public static void main(String[] args)
+    {
+        helloProcessing();
+
+>>>>>>> f48aec3078eb59df87cccb1b9fae4670b81401f3
         // Tara Misty
         // Tara Tara
         
